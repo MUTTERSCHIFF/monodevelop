5952d81d27dfa30f1a85a2af0d28decbf4e5c51c
Jose Miguel Torres <josemiguel@xamarin.com>
2018-02-12 08:14:19 +0100

    [Ide] Fixes a few issues
    - Code clean up
    - Removes unnecessary toolbar spaces

--- main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.BuildOutputView/BuildOutputWidget.cs
+++ main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.BuildOutputView/BuildOutputWidget.cs
@@ -126,7 +126,6 @@ namespace MonoDevelop.Ide.BuildOutputView
 
 			toolbar = new DocumentToolbar ();
 
-			toolbar.AddSpace ();
 			box = new Gtk.VBox ();
 			toolbar.Add (box, true);
 
@@ -175,7 +174,7 @@ namespace MonoDevelop.Ide.BuildOutputView
 
 		void TreeView_SelectionChanged (object sender, EventArgs e)
 		{
-			var selectedNode = (sender as Xwt.TreeView).SelectedRow as BuildOutputNode;
+			var selectedNode = treeView.SelectedRow as BuildOutputNode;
 			if (selectedNode == null)
 				return;
 
